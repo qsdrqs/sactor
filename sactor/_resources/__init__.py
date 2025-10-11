7cc54121e7a96bc21ccfe3ffb812901d8a90b8a5
qsdrqs <qsdrqs@gmail.com>
2025-10-11 14:01:51 -0500

    support pip package

---
+++ sactor/_resources/__init__.py
@@ -0,0 +1,6 @@
+"""Bundled static assets packaged with sactor.
+
+Currently exposes default configuration templates and proc-macro sources
+needed when running from an installed wheel where the repository checkout
+is not available.
+"""
